187afaad3e2833331a14f075997dac2037d00363
GABRIEL VARUZZA MALTA <gabriel.vmalta@senacsp.edu.br>
2022-08-18 20:05:11 -0300

    Criação do banco de dados e inicializador.

---
+++ PizzariaMVC/Data/InicializadorDeDados.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using PizzariaMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PizzariaMVC.Data
+{
+    public class InicializadorDeDados
+    {
+        public static void Inicializar(IApplicationBuilder builder)
+        {
+            using (var serviceScope = builder.ApplicationServices.CreateScope())
+            {
+                var context = serviceScope
+                    .ServiceProvider
+                    .GetService<PizzariaDbContext>();
+
+                context.Database.EnsureCreated();
+
+                if (!context.Pizzas.Any())
+                {
+                    context.Pizzas.AddRange(new List<Pizza>()
+                    {
+                        new Pizza("https://www.picanhacia.com.br/wp-content/uploads/2017/01/frango-c-catupiry.jpg", "Frango com Catupiry", 59, 2),
+                    });
+                }
+
+                if (!context.Sabores.Any())
+                {
+                    context.Sabores.AddRange(new List<Sabor>()
+                    {
+                        new Sabor("https://www.papodegordo.com.br/wp-content/uploads/2010/02/frangodesfiado2.jpg", "Frango"),
+                    });
+                }
+
+                if (!context.Tamanhos.Any())
+                {
+                    context.Tamanhos.AddRange(new List<Tamanho>()
+                    {
+                        new Tamanho("Pequeno"),
+                        new Tamanho("Médio"),
+                        new Tamanho("Grande"),
+                    });
+                }
+
+                if (!context.PizzasSabores.Any())
+                {
+                    context.PizzasSabores.AddRange(new List<PizzaSabor>()
+                    {
+                        new PizzaSabor(1, 1),
+
+                    });
+                }
+            }   
+        }
+    }
+}
---
+++ PizzariaMVC/Data/PizzariaDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PizzariaMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PizzariaMVC.Data
+{
+    public class PizzariaDbContext : DbContext
+    {
+        public PizzariaDbContext(DbContextOptions<PizzariaDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<PizzaSabor>().HasKey(ps => new
+            {
+                ps.PizzaId,
+                ps.SaborId
+            });
+        }
+
+        public DbSet<Pizza> Pizzas { get; set; }
+        public DbSet<Sabor> Sabores { get; set; }
+        public DbSet<Tamanho> Tamanhos { get; set; }
+        public DbSet<PizzaSabor> PizzasSabores { get; set; }
+    }
+}
