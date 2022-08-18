ffb59ebdf9ef9293960f4c57d2e53521cd57b389
GABRIEL VARUZZA MALTA <gabriel.vmalta@senacsp.edu.br>
2022-08-18 19:43:25 -0300

    Primeiro commmit, projeto vazio.

---
+++ PizzariaMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PizzariaMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
