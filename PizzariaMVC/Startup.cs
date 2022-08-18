3d3dbc14a98924eacda4a7008ebc1f24f3af5f2a
GABRIEL VARUZZA MALTA <gabriel.vmalta@senacsp.edu.br>
2022-08-18 20:20:25 -0300

    Criação do banco de dados.

--- PizzariaMVC/Startup.cs
+++ PizzariaMVC/Startup.cs
@@ -1,9 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using PizzariaMVC.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,6 +25,9 @@ namespace PizzariaMVC
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<PizzariaDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("SqlServer")));
+
             services.AddControllersWithViews();
         }
 
@@ -52,6 +57,8 @@ namespace PizzariaMVC
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
+
+            InicializadorDeDados.Inicializar(app);
         }
     }
 }
